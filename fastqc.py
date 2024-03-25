1435b2661f68699b70eb971b3ea1c016b8419fc9
Ivarlottman <ilottman@st.hanze.nl>
2024-03-25 09:40:52 +0100

    pep8 whitespace fix git status# clear# On branch main ok# Your branch is ahead of 'origin/main' by 1 commit.#   (use "git push" to publish your local commits)

--- fastqc.py
+++ fastqc.py
@@ -6,15 +6,15 @@ def run_fastqc(filename):
     path_fastqc = r"\tools\fastqc_v0.12.1\FastQC"
     path_output = r"\file_uploading\\"
 
-    command = ["java", 
-                "-Xmx250m", 
-                "-Dfastqc.unzip=true", 
-                "-Dfastqc.delete=true", 
+    command = ["java",
+                "-Xmx250m",
+                "-Dfastqc.unzip=true",
+                "-Dfastqc.delete=true",
                 r"-Dfastqc.output_dir=" + path_root + path_output,
-                "-classpath", 
-                ".;./sam-1.103.jar;./jbzip2-0.9.jar", 
-                "uk.ac.babraham.FastQC.FastQCApplication",  
-                path_root + path_output +  "SRR18574453.fastq"]#filename]
+                "-classpath",
+                ".;./sam-1.103.jar;./jbzip2-0.9.jar",
+                "uk.ac.babraham.FastQC.FastQCApplication",
+                path_root + path_output + "SRR18574453.fastq"] #filename]
     print(path_root + path_output)
 
     os.chdir(path_root + path_fastqc)
