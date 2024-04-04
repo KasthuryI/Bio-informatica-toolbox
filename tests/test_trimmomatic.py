10827c7bd007df5182ad40aad1e23889a02dc0c9
Ivarlottman <ilottman@st.hanze.nl>
2024-04-04 12:07:55 +0200

    updated test parameters

--- tests/test_trimmomatic.py
+++ tests/test_trimmomatic.py
@@ -16,20 +16,20 @@ def test_command():
     function from the trimmomatic.py module.
     """
     path_root = os.getcwd()
-    path_output = r"\trimmomatic_output"
+    path_output = r"/trimmomatic_output"
 
-    test1 = Trimmomatic("10", "20", "examplefile.file")
-    test2 = Trimmomatic("50", "25", "anotherexample.file")
+    test1 = Trimmomatic("3", "3", "4:15", "10", "20", "examplefile.file")
+    test2 = Trimmomatic("3", "3", "4:15", "50", "25", "anotherexample.file")
 
-    expected = ["java", "-jar", path_root + r"\tools\Trimmomatic-0.39\trimmomatic-0.39.jar", "SE",
-    path_root + r"\file_uploading" + "\\" + test1.filename,
-    path_root + path_output + r"\OUTPUT.fq",
-    "ILLUMINACLIP:TruSeq3-SE:2:30:10", "LEADING:3", "TRAILING:3", test1.minlen, test1.crop]
+    expected = [ "java", "-jar", path_root + r"/tools/Trimmomatic-0.39/trimmomatic-0.39.jar",
+        "SE", path_root + r"/file_uploading" + "/" + test1.filename,
+        path_root + path_output + r"/OUTPUT.fq", test1.illuminaclip, test1.leading,
+        test1.trailing, test1.sliding, test1.minlen, test1.crop]
 
-    expected2 = ["java", "-jar", path_root + r"\tools\Trimmomatic-0.39\trimmomatic-0.39.jar", "SE",
-    path_root + r"\file_uploading" + "\\" + test2.filename,
-    path_root + path_output + r"\OUTPUT.fq",
-    "ILLUMINACLIP:TruSeq3-SE:2:30:10", "LEADING:3", "TRAILING:3", test2.minlen, test2.crop]
+    expected2 = [ "java", "-jar", path_root + r"/tools/Trimmomatic-0.39/trimmomatic-0.39.jar",
+        "SE", path_root + r"/file_uploading" + "/" + test2.filename,
+        path_root + path_output + r"/OUTPUT.fq", test2.illuminaclip, test2.leading,
+        test2.trailing, test2.sliding, test2.minlen, test2.crop]
 
     cmd = test1.command_trimmomatic()
     assert cmd == expected
